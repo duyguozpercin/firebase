0aaee7b95cc1ab1018c37a4649103a8c67e4c7cf
duyguozpercin <duygucetind@gmail.com>
2025-05-28 14:20:54 +0300

    feat: integrate Firebase into the application
    - Added Firebase SDK as a dependency in package.json
    - Updated App component to display a simple message
    - Removed unused logo.svg file
    - Created firebase configuration file to initialize Firebase app and analytics

---
+++ src/config/firebase.js
@@ -0,0 +1,17 @@
+
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBEWiMm7cWevCaS9kGeYFAW7QPehIu6IrE",
+  authDomain: "fir-51810.firebaseapp.com",
+  projectId: "fir-51810",
+  storageBucket: "fir-51810.firebasestorage.app",
+  messagingSenderId: "132504065607",
+  appId: "1:132504065607:web:ebf325d0cb88e74ed88362",
+  measurementId: "G-BXEYVFMVGY"
+};
+
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
