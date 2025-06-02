1e66478299d0e3919adfe60140502987c7094591
duyguozpercin <duygucetind@gmail.com>
2025-06-03 01:07:31 +0300

    feat: initialize movie list state and set up useEffect for data fetching

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
 import { Auth } from "./components/auth";
+import {} from './config/firebase';
+import {db} from './config/firebase';
+import {getDocs} from 'firebase/firestore';
+
 
 function App() {
+  const [movieList, setMovieList] = useState([]);
+
+  useEffect(() => {
+    const getMovieList = async () => {
+      // Read the data
+      //set the movie list
+    }
+  }, [])
+  
   return (<div className="App">
     <Auth />
-
   </div>
   );
 }
