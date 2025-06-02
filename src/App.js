6f941767ee8979b4dd28d6a2bab6807d07a5aaad
duyguozpercin <duygucetind@gmail.com>
2025-06-03 01:12:45 +0300

    feat: set up movie collection reference and update data fetching logic

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@ import './App.css';
 import { Auth } from "./components/auth";
 import {} from './config/firebase';
 import {db} from './config/firebase';
-import {getDocs} from 'firebase/firestore';
+import {getDocs, collection} from 'firebase/firestore';
 
 
 function App() {
   const [movieList, setMovieList] = useState([]);
 
+  const moviesCollectionRef = collection(db, 'movies');
+
   useEffect(() => {
     const getMovieList = async () => {
       // Read the data
       //set the movie list
+      const dsata = await getDocs(moviesCollectionRef);
     }
   }, [])
   
